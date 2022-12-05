371183f7719976b9d4e2c6dcc3373ded4768c021
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2022-12-05 08:00:32 +0100

    Day 04: Adding a custom head.js for the about page

---
+++ app/about/head.js
@@ -0,0 +1,11 @@
+export default function AboutHead() {
+    return (
+        <>
+            <title>About</title>
+            <meta
+                name="description"
+                content="Marvin is building and designing software produts worldwide"
+            />
+        </>
+    )
+}
