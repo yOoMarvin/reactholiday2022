0f9424f0e34bf1040078c61fc6c809d3bade056c
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2022-12-18 08:32:37 +0100

    refactoring pokemon links

--- app/pokeapp/page.js
+++ app/pokeapp/page.js
@@ -1,18 +1,20 @@
-import NavLink from '/components/NavLink'
+import Link from 'next/Link'
+
 export default async function PokeappPage() {
     let res = await fetch('https://pokeapi.co/api/v2/pokemon')
     let data = await res.json()
 
     return (
         <main>
-            <div className="flex flex-col space-y-2">
+            <div className="grid grid-cols-6 gap-4">
                 {data.results.map((pokemon) => (
-                    <NavLink
+                    <Link
                         key={pokemon.name}
                         href={'/pokeapp/' + pokemon.name}
+                        className=" rounded-md bg-white bg-opacity-5 p-4 text-center transition-all duration-300 ease-out hover:bg-opacity-10 hover:text-blue-500"
                     >
                         <span className="capitalize">{pokemon.name}</span>
-                    </NavLink>
+                    </Link>
                 ))}
             </div>
         </main>
